f1d6821233299f061806e692ce5053c2c56d7823
dizcza <dizcza@gmail.com>
2018-09-08 22:04:43 +0300

    loss epoch and batch different

--- loss.py
+++ loss.py
@@ -1,11 +1,13 @@
-from abc import ABC
 import math
+from abc import ABC
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from constants import MAX_L0_DIST
+from monitor.batch_timer import timer
 
 
 class ContrastiveLoss(nn.Module, ABC):
@@ -79,39 +81,61 @@ class ContrastiveLossBatch(ContrastiveLoss):
         n_take = math.ceil(len(distances) / 2.)
         return distances[: n_take].mean()
 
-    def forward(self, outputs, labels):
+    def forward_random(self, outputs, labels):
         dist_same = []
         dist_other = []
-        outputs_sorted = {}
         labels_unique = labels.unique()
+        for label_id, label_same in enumerate(labels_unique):
+            mask_same = labels == label_same
+            outputs_same_label = outputs[mask_same]
+            n_same = len(outputs_same_label)
+            if n_same > 0:
+                dist = self.distance(outputs_same_label[1:], outputs_same_label[:-1], is_same=True)
+                dist_same.append(dist)
+
+            other_idx = np.arange(len(labels))[~mask_same]
+            other_idx = np.random.choice(other_idx, size=n_same, replace=True)
+            other_idx = torch.as_tensor(other_idx, device=outputs.device)
+            outputs_other_label = outputs[other_idx]
+            dist = self.distance(outputs_other_label, outputs_same_label, is_same=False)
+            dist_other.append(dist)
+        return dist_same, dist_other
+
+    def forward_pairwise(self, outputs, labels):
+        dist_same = []
+        dist_other = []
+        labels_unique = labels.unique()
+        outputs_sorted = {}
         for label in labels_unique:
             outputs_sorted[label.item()] = outputs[labels == label]
         for label_id, label_same in enumerate(labels_unique):
             outputs_same_label = outputs_sorted[label_same.item()]
             n_same = len(outputs_same_label)
-            if n_same > 1:
-                dist = self.distance(outputs_same_label[1:], outputs_same_label[:-1], is_same=True)
-                dist_same.append(dist)
+            upper_triangle_idx = np.triu_indices(n=n_same, k=1)
+            upper_triangle_idx = torch.as_tensor(upper_triangle_idx, device=outputs_same_label.device)
+            same_left, same_right = outputs_same_label[upper_triangle_idx]
+            dist_same.append(self.distance(same_left, same_right, is_same=True))
 
-            if not self.same_only:
-                for label_other in labels_unique[label_id+1:]:
-                    outputs_other_label = outputs_sorted[label_other.item()]
-                    n_other = len(outputs_other_label)
-                    n_max = max(n_same, n_other)
-                    idx_same = torch.arange(n_max) % n_same
-                    idx_other = torch.arange(n_max) % n_other
-                    dist = self.distance(outputs_other_label[idx_other], outputs_same_label[idx_same], is_same=False)
-                    dist = dist[dist > self.eps]
-                    dist_other.append(dist)
+            for label_other in labels_unique[label_id + 1:]:
+                outputs_other_label = outputs_sorted[label_other.item()]
+                n_other = len(outputs_other_label)
+                n_max = max(n_same, n_other)
+                idx_same = torch.arange(n_max) % n_same
+                idx_other = torch.arange(n_max) % n_other
+                dist = self.distance(outputs_other_label[idx_other], outputs_same_label[idx_same], is_same=False)
+                dist_other.append(dist)
+        return dist_same, dist_other
 
-        # loss_same = self.half_majority_mean(torch.cat(dist_same))
-        loss_same = torch.cat(dist_same).mean()
-        dist_other = tuple(filter(len, dist_other))
-        if len(dist_other) > 0:
-            loss_other = torch.cat(dist_other).mean()
-            # loss_other = self.half_majority_mean(torch.cat(dist_other))
+    def forward(self, outputs, labels):
+        if timer.is_epoch_finished():
+            dist_same, dist_other = self.forward_random(outputs, labels)
         else:
-            loss_other = 0
+            dist_same, dist_other = self.forward_pairwise(outputs, labels)
+
+        loss_same = torch.cat(dist_same).mean()
+        dist_other = torch.cat(dist_other)
+        dist_other = dist_other[dist_other > self.eps]
+        loss_other = dist_other.mean()
         loss = loss_same + loss_other
 
         return loss
