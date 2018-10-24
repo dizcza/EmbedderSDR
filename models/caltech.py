3c5de6e1661751204f5b9b6d890983a814b6a243
dizcza <dizcza@gmail.com>
2018-10-24 11:30:15 +0300

    models.caltech forget call requires_grad false

--- models/caltech.py
+++ models/caltech.py
@@ -11,34 +11,41 @@ def _classifier(in_features: int, kwta: Optional[nn.Module]):
         return nn.Linear(in_features=in_features, out_features=256)
 
 
+def make_no_grad(model):
+    for param in model.parameters():
+        param.requires_grad_(False)
+
+
 def resnet18(pretrained=True, kwta=None):
     model = torchvision.models.resnet18(pretrained=pretrained)
+    make_no_grad(model)
     model.fc = _classifier(in_features=512, kwta=kwta)
     return model
 
 
 def squeezenet1_1(pretrained=True, kwta=None):
     model = torchvision.models.squeezenet1_1(pretrained=pretrained)
-    for param in model.parameters():
-        param.requires_grad_(False)
+    make_no_grad(model)
     model.num_classes = 256
     final_conv = nn.Conv2d(512, model.num_classes, kernel_size=1)
+    nn.init.normal_(final_conv.weight, mean=0.0, std=0.01)
     classifier = [nn.Dropout(p=0.5), final_conv, nn.ReLU(inplace=True), nn.AvgPool2d(13, stride=1)]
     if kwta:
         classifier.append(kwta)
     model.classifier = nn.Sequential(*classifier)
-    nn.init.normal_(final_conv.weight, mean=0.0, std=0.01)
     return model
 
 
 def densenet121(pretrained=True, kwta=None):
     model = torchvision.models.densenet121(pretrained=pretrained)
+    make_no_grad(model)
     model.classifier = _classifier(in_features=1024, kwta=kwta)
     return model
 
 
 def vgg19(pretrained=True, kwta=None):
     model = torchvision.models.vgg19(pretrained=pretrained)
+    make_no_grad(model)
     classifier = [*model.classifier]
     last_layer = classifier.pop()  # 4096
     model.classifier = _classifier(in_features=last_layer.in_features, kwta=kwta)
