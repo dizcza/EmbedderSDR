bc63112e9fecf21aad01c51e808c673f9fad11c6
dizcza <dizcza@gmail.com>
2018-11-24 21:54:20 +0200

    forgot to add relu in MLP

--- models/mlp.py
+++ models/mlp.py
@@ -8,10 +8,14 @@ class MLP(nn.Module):
         :param fc_sizes: fully-connected layer sizes
         """
         super().__init__()
+        fc_sizes = list(fc_sizes)
+        n_classes = fc_sizes.pop()
         classifier = []
         for in_features, out_features in zip(fc_sizes[:-1], fc_sizes[1:]):
             linear = nn.Linear(in_features=in_features, out_features=out_features)
             classifier.append(linear)
+            classifier.append(nn.ReLU(inplace=True))
+        classifier.append(nn.Linear(in_features=fc_sizes[-1], out_features=n_classes))
         self.classifier = nn.Sequential(*classifier)
 
     def forward(self, x):
