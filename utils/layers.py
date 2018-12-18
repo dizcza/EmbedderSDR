604bbff20a417087bc4392677c5e9cfd9e565d74
dizcza <dizcza@gmail.com>
2018-12-18 09:51:49 +0200

    fixed breaking change in _load_from_state_dict

--- utils/layers.py
+++ utils/layers.py
@@ -51,7 +51,8 @@ class SerializableModule(nn.Module, ABC):
             destination[prefix + attribute] = getattr(self, attribute)
         return destination
 
-    def _load_from_state_dict(self, state_dict, prefix, metadata, strict, missing_keys, unexpected_keys, error_msgs):
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys,
+                              error_msgs):
         state_dict_keys = list(state_dict.keys())
         for attribute in self.state_attr:
             key = prefix + attribute
@@ -59,5 +60,6 @@ class SerializableModule(nn.Module, ABC):
                 setattr(self, attribute, state_dict.pop(key))
             elif strict:
                 missing_keys.append(key)
-        super()._load_from_state_dict(state_dict=state_dict, prefix=prefix, metadata=metadata, strict=strict,
-                                      missing_keys=missing_keys, unexpected_keys=unexpected_keys, error_msgs=error_msgs)
+        super()._load_from_state_dict(state_dict=state_dict, prefix=prefix, local_metadata=local_metadata,
+                                      strict=strict, missing_keys=missing_keys, unexpected_keys=unexpected_keys,
+                                      error_msgs=error_msgs)
